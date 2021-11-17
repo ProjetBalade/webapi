932165ec85381294493e772d671a096a28be04a0
Nicolas.Delcommune <nicodelco@hotmail.be>
2021-11-17 10:13:05 +0100

    Database init

---
+++ Infrastructure/Database.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.SqlClient;
+
+namespace Infrastructure
+{
+    public class Database
+    {
+        private const string ConnectionString = "Server=DESKTOP-L97KEOS;Database=dbBalade;Integrated Security=SSPI";
+
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(ConnectionString);
+        }
+    }
+}
